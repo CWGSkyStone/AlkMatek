51a09a48c0449463f3653a4cfbec4b75c25f58d3
Szabó József <SkyStone213@gmail.com>
2026-02-02 15:41:21 +0100

    throwDice refactor

--- src/Poker.java
+++ src/Poker.java
@@ -1,22 +1,15 @@
 import java.util.Random;
 
 public class Poker {
-    static Random random = new Random();
     protected static void play() {
-        machineThrow();
-        playerThrow();
+        throwDice("Gép");
+        throwDice("\nJátekos");
     }
-    private static void machineThrow(){
-        System.out.println("A gép dobásai: ");
+    private static void throwDice(String role){
+        Random random = new Random();
+        System.out.println(role +" dobásai: ");
         for(int i = 1; i <= 5; i++) {
             System.out.print(random.nextInt(6) + 1 + " ");
+        }
     }
-}
-    private static void playerThrow(){
-
-        System.out.println("\nA te dobásaid: ");
-        for(int i = 1; i <= 5; i++) {
-            System.out.print(random.nextInt(6) + 1 + " ");
-    }
-}
 }
